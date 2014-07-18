8ad4bd55db5b836baa5dca47f94b9671c4bbc428
Gunnar Tiedt <gunnar.tiedt@edeka.de>
2014-07-18 10:54:46 +0200

    own Prompt

--- git-prompt-colors.sh
+++ git-prompt-colors.sh
@@ -19,10 +19,10 @@ GIT_PROMPT_CLEAN="${BoldGreen}✔"      # a colored flag indicating a "clean" re
 GIT_PROMPT_COMMAND_OK="${Green}✔ "    # indicator if the last command returned with an exit code of 0
 GIT_PROMPT_COMMAND_FAIL="${Red}✘ "   # indicator if the last command returned with an exit code of other than 0
 
-GIT_PROMPT_START_USER="${Yellow}${PathShort}${ResetColor}"
-GIT_PROMPT_START_ROOT="${Yellow}${PathShort}${ResetColor}"
-GIT_PROMPT_END_USER=" \n${White}${Time12a}${ResetColor} $ "
-GIT_PROMPT_END_ROOT=" \n${White}${Time12a}${ResetColor} # "
+GIT_PROMPT_START_USER="${Blue}\u${ResetColor}@\h ${Yellow}[${PathShort}]${ResetColor}"
+GIT_PROMPT_START_ROOT="${Red}\u${ResetColor}@\h ${Yellow}[${PathShort}]${ResetColor}"
+GIT_PROMPT_END_USER="${ResetColor} \$ "
+GIT_PROMPT_END_ROOT="${ResetColor} \$ "
 
 # Please do not add colors to these symbols
 GIT_PROMPT_SYMBOLS_AHEAD="↑·"         # The symbol for "n versions ahead of origin"
